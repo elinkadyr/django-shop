354a0d4def9cc34a4520b1bfd16d5ce7ccb2b37e
Elina <elinkdrva@gmail.com>
2023-04-27 12:15:04 +0600

    comment, rating, favorite, pagination

---
+++ review/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CommentViewSet, FavoriteViewSet, AddRatingAPIView
+
+
+router = DefaultRouter()
+router.register("comments", CommentViewSet)
+router.register("favorites", FavoriteViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("rating/", AddRatingAPIView.as_view()),
+]
--- review/views.py
+++ review/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from .models import Comment, Favorite, Rating
+from .serializers import CommentSerializer, FavoriteSerializer, RatingSerializer
 
-# Create your views here.
+
+class CommentViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, GenericViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class FavoriteViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, GenericViewSet):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class AddRatingAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(request_body=RatingSerializer())
+    def post(self, request):
+        ser = RatingSerializer(data=request.data, context={"request": request})
+        ser.is_valid(raise_exception=True)
+        ser.save()
+        return Response(ser.data, status=201)
+
+
+
